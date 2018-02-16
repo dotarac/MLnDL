7c644cb16b47c27ecbee6816e82c88c8c66bab1f
yagamiash <ashobot@gmail.com>
2018-02-16 11:52:18 +0530

    Added Shallow Neural Nets model

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from models import LogisticRegression
+from models import LogisticRegression,ShallowNeuralNet
 
+# Data extraction and test-train(dev) split 
 dataset='iris.csv'
 df=pd.read_csv(dataset)
 y=df['label'].values
@@ -11,10 +12,18 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.33,random_state=4
 X_train,X_test,y_train,y_test=X_train.T,X_test.T,y_train.reshape(len(y_train),1).T,y_test.reshape(len(y_test),1).T
 
 
-
+# Logistic regression
 Lor=LogisticRegression(X_train,X_test,y_train,y_test)
 W,b,costs=Lor.gradient_descent_train()
 Lor.plot_costs()
 y_pred=Lor.predict()
 Lor.accuracy_metrics(y_pred)
-				
+
+# 2 layer Neural Net with 5 neurons in hidden layer
+nn=ShallowNeuralNet(X_train,X_test,y_train,y_test)
+W1,b1,W2,b2,J=nn.gradient_descent_train()
+nn.plot_costs()
+y_pred=nn.predict()
+nn.accuracy_metrics(y_pred)
+
+# Deep Neural net with multiple hidden layers
--- models.py
+++ models.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class LogisticRegression:
-	'Perform Logistic Regression for binary classification'
+	'Logistic Regression for binary classification'
 	def __init__(self,X_train,X_test,y_train,y_test,n_epochs=1000,alpha=0.03):
 		
 		self.X_train=X_train
@@ -74,13 +74,88 @@ class LogisticRegression:
 			plt.plot(range(self.n_epochs),self.costs)
 			plt.show()	
 
+	
+class ShallowNeuralNet:
+	'2 layer Neural Network for binary classification'
+	def __init__(self,X_train,X_test,y_train,y_test,h_neurons=5,n_epochs=1000,alpha=0.03):
+		self.X_train=X_train
+		self.X_test=X_test
+		self.y_test=y_test
+		self.y_train=y_train
+		self.n_epochs=n_epochs
+		self.alpha=alpha
+		self.h_neurons=h_neurons
+		self.W1=np.random.randn(h_neurons,len(self.X_train))*0.01
+		self.b1=np.zeros((h_neurons,1))
+		self.W2=np.random.randn(1,h_neurons)*0.01
+		self.b2=np.zeros((1,1))
+		self.costs=[]
 
+	def sigmoid(self,z):
+		'''sigmoid activation Function'''
+		return(1/(1+np.exp(-z)))
 
+	def tanh(self,z):	
+		'''tanh activation function'''
+		return((np.exp(z)-np.exp(-z))/(np.exp(z)+np.exp(-z)))
+		
+	def forward_propogate(self):
+		'''Forward propogating the activations'''
+		Z1=np.dot(self.W1,self.X_train)+self.b1
+		A1=self.tanh(Z1)
+		Z2=np.dot(self.W2,A1)+self.b2
+		A2=self.sigmoid(Z2)
+		return A1,A2	
 
+	def backward_propogate(self,A1,A2):
+		'''Back propogating the gradients'''
+		m=self.X_train.shape[1]
+		dZ2=A2-self.y_train
+		dW2=np.dot(dZ2,A1.T)/m
+		db2=np.sum(dZ2,axis=1,keepdims=True)/m
+		dZ1=np.dot(self.W2.T,dZ2)*(1-np.power(A1,2))
+		dW1=np.dot(dZ1,self.X_train.T)/m
+		db1=np.sum(dZ1,axis=1,keepdims=True)/m
+		J=np.squeeze(-np.sum(np.multiply(np.log(A2),self.y_train)+np.multiply(np.log(1-A2),1-self.y_train))/m)
+		return dW1,db1,dW2,db2,J
 
-			
+	def gradient_descent_train(self):
+		for i in range(self.n_epochs):
+			A1,A2=self.forward_propogate()
+			dW1,db1,dW2,db2,J=self.backward_propogate(A1,A2)
+			self.W1-=dW1*self.alpha			
+			self.b1-=db1*self.alpha
+			self.W2-=dW2*self.alpha
+			self.b2-=db2*self.alpha
+			self.costs.append(J)	
+		return self.W1,self.b1,self.W2,self.b2,self.costs
+		
+	def predict(self):
+		'''predict new class labels'''
+		Z1=np.dot(self.W1,self.X_test)+self.b1
+		A1=self.tanh(Z1)
+		Z2=np.dot(self.W2,A1)+self.b2
+		A2=self.sigmoid(Z2).flatten()
+		A=np.array([0 if i<0.5 else 1 for i in A2])
+		return A
 
+	def accuracy_metrics(self,y_pred):
+		'''performance measurements of algorithm'''
+		stats={}
+		diff=list(y_pred-self.y_test.flatten())
+		sums=list(y_pred+self.y_test.flatten())
+		stats["test_accuracy"]=diff.count(0)/float(len(diff))
+		stats["miss_rate"]=1-stats["test_accuracy"]
+		stats["confusion_matrix"]=[[sums.count(0),diff.count(-1)],[diff.count(1),sums.count(2)]]
+		print(stats) 
 
+	def plot_costs(self):
+		'''plot cost-function curve'''
+		if self.costs==[]:
+			print("Run Predict first to avail costs\n")
+		else:
+			plt.plot(range(self.n_epochs),self.costs)
+			plt.show()		
 
 
 
